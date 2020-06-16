e1ddb50b7ee6151ff8d59d79cea098e769b393bb
zfelipesilveira <zfelipesilveira@gmail.com>
2020-06-16 01:11:53 -0300

    dica para fazer a leitura do txt

--- src/LeituraTexto.java
+++ src/LeituraTexto.java
@@ -11,6 +11,17 @@ public class LeituraTexto {
 
 
     //USAR O MÉTODO DA CLASSE STRING PARA VERIFICAR O PRIMEIRO CARACTER DE CADA LINHA
+
+//    DICA:
+//    Você não vai conseguir ler e processar todas as linhas da mesma maneira.
+//    Será necessário que o seu 'parser' leia o arquivo-texto linha-a-linha.
+//    Para cada linha precisa verificar se é um comentário, uma seção, uma linha em branco ou dados - pelo primeiro caracter.
+//            Se for uma linha com dados, você poderá utilizar o useDelimiter.
+//    Se for uma seção, você precisa eliminar o abre e fecha colchetes.
+//    Veja na API Java, que a classe String possui métodos para manipular os seus caracteres e partes da String (substring).
+//
+//    Prof. Yamaguti
+
     public void leituraArquivo() {
         Path path = Paths.get("CLIENTES.TXT");
         try (BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset())) {
